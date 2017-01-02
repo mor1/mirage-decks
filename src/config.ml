4de83bb3f08f626baed1ae803e8f9c8c78a37f5f
Richard Mortier <mort@cantab.net>
2017-01-02 20:26:37 +0000

    remove inoperable constraint

--- src/config.ml
+++ src/config.ml
@@ -17,8 +17,7 @@ let stackv4 = generic_stackv4 default_console tap0
 let httpsvr = http_server @@ conduit_direct ~tls:false stackv4
 
 let libraries = [ "tyxml"; "mirage-http"; "logs"; "mirage-logs"; "magic-mime" ]
-let packages =
-  [ "tyxml>=4.0.0"; "mirage-http"; "logs"; "mirage-logs"; "magic-mime" ]
+let packages = [ "tyxml"; "mirage-http"; "logs"; "mirage-logs"; "magic-mime" ]
 
 let http_job =
   foreign ~libraries ~packages ~keys "Server.Main"
